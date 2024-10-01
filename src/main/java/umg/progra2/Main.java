63b0bdc2d004c170aa42862b5f8d2ddad645d455
Tulio <tquintanaa@miumg.edu.gt>
2024-10-01 16:35:27 -0600

    ya mero

--- src/main/java/umg/progra2/Main.java
+++ src/main/java/umg/progra2/Main.java
@@ -1,17 +1,24 @@
 package umg.progra2;
 
+import org.matheclipse.core.eval.ExprEvaluator;
+import org.matheclipse.core.interfaces.IExpr;
+import org.matheclipse.parser.client.SyntaxError;
+import org.matheclipse.parser.client.math.MathException;
+import umg.progra2.Procesos.Integrales;
+
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    static Integrales integrales = new Integrales();
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingresa la integral que deseas resolver");
+        String expression = sc.nextLine();
+        String[] partes = expression.split(" ");
+        char diferencial = partes[1].charAt(1);
+        String termino = partes[0];
+        System.out.println("El resultado es: "+integrales.ResolverIntegral(termino,diferencial));
     }
 }
--- src/main/java/umg/progra2/Procesos/Integrales.java
+++ src/main/java/umg/progra2/Procesos/Integrales.java
@@ -1,2 +1,58 @@
-package umg.progra2.Procesos;public class Integrales {
+package umg.progra2.Procesos;
+
+import org.matheclipse.core.eval.ExprEvaluator;
+import org.matheclipse.core.interfaces.IExpr;
+import org.matheclipse.parser.client.SyntaxError;
+import org.matheclipse.parser.client.math.MathException;
+
+public class Integrales {
+
+    public  String ResolverIntegral(String termino,char diferencial){
+        ExprEvaluator resolver = new ExprEvaluator();
+        String resultado = "";
+        try {
+            String integral1 = "Integrate["+termino+","+diferencial+"]";
+            IExpr result = resolver.eval(integral1);
+            resultado= result.toString();
+        } catch (SyntaxError e){
+            System.err.println("Error de sintaxis: " + e.getMessage());
+        } catch (MathException e) {
+            System.err.println("Error matemático: " + e.getMessage());
+        }
+        return resultado;
+    }
+
+
+
+//    ExprEvaluator util = new ExprEvaluator();
+//
+//        try {
+//        // Ejemplo 1: Integral de sin^2(x)
+//        String integral1 = "Integrate[Sin[2x]^3*Cos[2x]^4,x]";
+//        IExpr result1 = util.eval(integral1);
+//        System.out.println("Integral de sin^2(x): " + result1.toString());
+//
+//        // Ejemplo 2: Integral de cos^3(x)
+//        String integral2 = "Integrate[Sin[5x]^4, x]";
+//        IExpr result2 = util.eval(integral2);
+//        System.out.println("Integral de cos^3(x): " + result2.toString());
+//
+//        // Ejemplo 3: Integral de tan^2(x)
+//        String integral3 = "Integrate[Tan[x]^2, x]";
+//        IExpr result3 = util.eval(integral3);
+//        System.out.println("Integral de tan^2(x): " + result3.toString());
+//
+//        // Ejemplo 4: Integral de sin^4(x) * cos^2(x)
+//        String integral4 = "Integrate[Sin[x]^4 * Cos[x]^2, x]";
+//        IExpr result4 = util.eval(integral4);
+//        System.out.println("Integral de sin^4(x) * cos^2(x): " + result4.toString());
+//
+//    } catch (SyntaxError e) {
+//        System.err.println("Error de sintaxis: " + e.getMessage());
+//    } catch (MathException e) {
+//        System.err.println("Error matemático: " + e.getMessage());
+//    }
+
+
+
 }
